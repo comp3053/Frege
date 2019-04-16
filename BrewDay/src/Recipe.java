074fbe8d10b7734681b06b834f282a2a251d6ed6
l630003065 <l630003065@CZ148059.UTC.local>
2019-04-16 16:30:53 +0800

    Upload Recipe.java

---
+++ BrewDay/src/Recipe.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Recipe {
+	private String recipeName;
+	private float quantity;
+	private String unit;
+	private ArrayList RecipeIngredient;
+	
+	public void setRecipeName(String name) {
+		this.recipeName = name;
+	}
+	
+	public String getRecipeName() {
+		return this.recipeName;
+	}
+	
+	public void setQuantity(float quantity) {
+		this.quantity = quantity;
+	}
+	
+	public float getQuantity() {
+		return this.quantity;
+	}
+	
+	public boolean setUnit(String unit) {
+		if (unit.equals("kg") || unit.equals("g") || unit.equals("ml") || unit.equals("l")) {
+			this.unit = unit;
+			return true;
+		}
+		return false;
+	}
+	
+	public String getUnit() {
+		return this.unit;
+	}
+	
+	public boolean updateIngredient(String ingreName, float amount) {
+		if ((amount >= 0) && (ingreName.equals("malts")||ingreName.equals("hops")||ingreName.equals("yeasts")||ingreName.equals("sugars")||ingreName.equals("additives"))) {
+			
+			return true;
+		}
+		return false;
+	}
+}
