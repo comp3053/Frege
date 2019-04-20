99d8b46986ef32eb3ba6683341b56404ac2b0cf9
guxianggao <guxianggao@qq.com>
2019-04-20 20:38:07 +0800

    Add Brew class

---
+++ src/Brew.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Brew {
+	private float batchSize;
+	private Date date;
+	
+	public float getBatchSize() {
+		return batchSize;
+	}
+	public void setBatchSize(float batchSize) {
+		this.batchSize = batchSize;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public void Brew(String recipeName) {
+		//unfinished method
+	}
+	public ArrayList checkRecipes() {
+		//unfinished method
+		return null;
+	}
+}
