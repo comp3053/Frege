53bce7678ce301cc4e177cc0e704d1fca0988fc3
CandiceCKX <ckxcandice@163.com>
2019-05-07 15:44:51 +0800

    Message popup for RecipesAddPage

--- src/UI/RecipesAddPage.java
+++ src/UI/RecipesAddPage.java
@@ -173,14 +173,18 @@ public class RecipesAddPage extends JFrame {
 		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				// 暂时空缺修改数据库的行为或页面
+				//Temporary vacancies modify the behavior or page of the database
 				//应当有一个提示add成功的页面
+				//There should be a page that prompts that add is successful
 				
 				// 如果操作成功，则修改数据库并且返回RecipePage
+				//If the operation is successful, modify the database and return to RecipePage
 				JOptionPane.showMessageDialog(null, "Success!");
 				closeThis();
 				new RecipePage();
 				
 				//如果操作失败，不改变数据库并且刷新当前页面
+				//If the operation fails, do not change the database and refresh the current page
 				/*JOptionPane.showMessageDialog(null,"Operation error!","Warning",JOptionPane.ERROR_MESSAGE);
 				closeThis();
 				new RecipesAddPage().setVisible(true);*/
