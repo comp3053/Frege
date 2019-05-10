bf4163287d827d769e7211952078fbadc5b206eb
CandiceCKX <ckxcandice@163.com>
2019-05-11 01:53:55 +0800

    Successful with SelectRecipesPage!

--- src/UI/SelectRecipesPage.java
+++ src/UI/SelectRecipesPage.java
@@ -1,14 +1,20 @@
 package UI;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
+
+import com.sun.glass.events.MouseEvent;
 
 import javafx.scene.control.TableColumn;
 
@@ -17,16 +23,20 @@ import javax.swing.SwingConstants;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Vector;
 
 import javax.swing.JLayeredPane;
 import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 
 public class SelectRecipesPage extends JFrame {
 
 	private JPanel contentPane;
 	private JTable table;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -43,6 +53,7 @@ public class SelectRecipesPage extends JFrame {
 			}
 		});
 	}
+	
 
 	/**
 	 * Create the frame.
@@ -65,23 +76,75 @@ public class SelectRecipesPage extends JFrame {
 		layeredPane.setBounds(14, 45, 554, 279);
 		contentPane.add(layeredPane);
 		
-		// Table
-		DefaultTableModel tableModel=new DefaultTableModel();
-		tableModel.addColumn("Recipes");
-        tableModel.addColumn("Operation");
-		table = new JTable(tableModel);
-		table.setFont(new Font("Calibri", Font.PLAIN, 18));
-		table.setBounds(75, 13, 394, 253);
-		//layeredPane.add(table);
-		/* 用JScrollPane装载JTable，这样超出范围的列就可以通过滚动条来查看 */  
-        JScrollPane scroll = new JScrollPane(table);  
-        scroll.setBounds(14, 325, 554, -275);
-        layeredPane.add(table);
-        getContentPane().add(scroll); 
-        String n = "k";
-  
+		/*// Brew Button
+		JButton btnBrew = new JButton("Brew");
+		btnBrew.setFont(new Font("Calibri", Font.PLAIN, 25));
+		btnBrew.setBounds(359, 183, 110, 43);
+		layeredPane.add(btnBrew);*/
 		
-       
+		// Table Model
+		// Rewrite the table can be select but can't be edited
+		DefaultTableModel tableModel=new DefaultTableModel() {
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};	
+		tableModel.addColumn("Recipes");
+        for(int i = 0;i < 10;i++) {
+        	// should link to database
+        	Vector v = new Vector();
+        	v.add("Recipe"+i);
+        	//v.add("");
+        	tableModel.addRow(v);
+        }
+        
+        // Table
+        table = new JTable(tableModel);
+		table.setFont(new Font("Calibri", Font.PLAIN, 20));
+		table.setRowHeight(30);
+		// Dynamic table 
+		JScrollPane scroll = new JScrollPane(table);
+		scroll.setEnabled(false);
+		scroll.setBounds(75, 13, 196, 253);
+		layeredPane.add(scroll);
+		table.setModel(tableModel);
+		
+		// Click the table, will choose one recipe to brew 
+		String inf;
+		table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+			int index;
+			@Override
+			public void valueChanged(ListSelectionEvent arg0) {
+				// TODO Auto-generated method stub
+				index = table.getSelectedRow();
+				//String n = (String) table.getValueAt(index, 0);
+				textField = new JTextField();
+				textField.setEditable(false);
+				textField.setHorizontalAlignment(SwingConstants.CENTER);
+				textField.setFont(new Font("Calibri", Font.PLAIN, 25));
+				textField.setBounds(332, 87, 141, 46);
+				textField.setText((String) table.getValueAt(index, 0));
+				layeredPane.add(textField);
+				textField.setColumns(10);
+				
+				// Brew Button
+				JButton btnBrew = new JButton("Brew");
+				btnBrew.setFont(new Font("Calibri", Font.PLAIN, 25));
+				btnBrew.setBounds(359, 183, 110, 43);
+				layeredPane.add(btnBrew);
+				// Brew Button Listener
+				btnBrew.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+	
+						closeThis();
+						new IngredientCheckPage().setVisible(true);;
+					}
+				});
+			}
+			 
+		});
+		
+		
 		
 		JButton btnCancel = new JButton("Cancel");
 		btnCancel.setBounds(215, 325, 150, 45);
@@ -98,4 +161,5 @@ public class SelectRecipesPage extends JFrame {
 	protected void closeThis() {
 		contentPane.setVisible(false);
 	}
+
 }
