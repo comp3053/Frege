1f2559e92b44550680492261cf7bd1b0344130a3
guxianggao <guxianggao@qq.com>
2019-05-11 14:52:17 +0800

    modify the RecipesUpdateController.java

--- src/Controller/RecipesUpdateController.java
+++ src/Controller/RecipesUpdateController.java
@@ -11,28 +11,6 @@ public class RecipesUpdateController {
 	}
 	
 	public boolean updateRecipe(String name, float maltQ, float hopQ, float yeastQ, float sugarQ, float additiveQ) {
-		ArrayList<RecipeIngredient> updateR = new ArrayList<RecipeIngredient>();
-		if(maltQ != 0) {
-			RecipeIngredient malt = new RecipeIngredient("malt", maltQ, "kg");
-			updateR.add(malt);
-		}
-		if(hopQ != 0) {
-			RecipeIngredient hop = new RecipeIngredient("hop", hopQ, "kg");
-			updateR.add(hop);
-		}
-		if(yeastQ != 0) {
-			RecipeIngredient yeast = new RecipeIngredient("yeast", yeastQ, "kg");
-			updateR.add(yeast);
-		}
-		if(sugarQ != 0) {
-			RecipeIngredient sugar = new RecipeIngredient("sugar", sugarQ, "kg");
-			updateR.add(sugar);
-		}
-		if(additiveQ != 0) {
-			RecipeIngredient additive = new RecipeIngredient("additive", additiveQ, "kg");
-			updateR.add(additive);
-		}
-		
-		return Recipe.updateRecipe();
+		return Recipe.updateRecipe(name, maltQ, hopQ, yeastQ, sugarQ, additiveQ);
 	} 
 }
