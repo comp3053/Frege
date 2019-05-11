401de9c8a49a85fc35a6325b54dfaaebd484c69d
guxianggao <guxianggao@qq.com>
2019-05-11 15:34:18 +0800

    modify the RecipesAddController.java

--- src/Controller/RecipesAddController.java
+++ src/Controller/RecipesAddController.java
@@ -10,26 +10,21 @@ public class RecipesAddController {
 	
 	public boolean addRecipe(String name, float maltQ, float hopQ, float yeastQ, float sugarQ, float additiveQ) {
 		ArrayList<RecipeIngredient> addR = new ArrayList<RecipeIngredient>();
-		if(maltQ != 0) {
-			RecipeIngredient malt = new RecipeIngredient("malt", maltQ, "kg");
-			addR.add(malt);
-		}
-		if(hopQ != 0) {
-			RecipeIngredient hop = new RecipeIngredient("hop", hopQ, "kg");
-			addR.add(hop);
-		}
-		if(yeastQ != 0) {
-			RecipeIngredient yeast = new RecipeIngredient("yeast", yeastQ, "kg");
-			addR.add(yeast);
-		}
-		if(sugarQ != 0) {
-			RecipeIngredient sugar = new RecipeIngredient("sugar", sugarQ, "kg");
-			addR.add(sugar);
-		}
-		if(additiveQ != 0) {
-			RecipeIngredient additive = new RecipeIngredient("additive", additiveQ, "kg");
-			addR.add(additive);
-		}
+		
+		RecipeIngredient malt = new RecipeIngredient("malt", maltQ, "kg");
+		addR.add(malt);
+		
+		RecipeIngredient hop = new RecipeIngredient("hop", hopQ, "kg");
+		addR.add(hop);
+		
+		RecipeIngredient yeast = new RecipeIngredient("yeast", yeastQ, "kg");
+		addR.add(yeast);
+		
+		RecipeIngredient sugar = new RecipeIngredient("sugar", sugarQ, "kg");
+		addR.add(sugar);
+		
+		RecipeIngredient additive = new RecipeIngredient("additive", additiveQ, "kg");
+		addR.add(additive);
 		
 		return Recipe.addRecipe(name, 1, "l",addR);
 	}
