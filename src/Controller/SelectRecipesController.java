7c1cb1a1e0e2c2ca80aa4cfab90fbb4a0e0ee95e
Yvonne <47659990+YvonneTech@users.noreply.github.com>
2019-05-12 14:02:37 +0800

    delete useless controller

--- src/Controller/SelectRecipesController.java
+++ src/Controller/SelectRecipesController.java
@@ -6,8 +6,4 @@ public class SelectRecipesController {
 	public SelectRecipesController() {
 		
 	}
-	
-	public boolean brewRecipe(String recipeName, float batchSize) {
-		return Brew.brew(recipeName, batchSize);
-	}
 }
