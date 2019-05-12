acb4fd3b8a7b85dfa1fb520c77ae0abb4a88ec9f
CandiceCKX <ckxcandice@163.com>
2019-05-12 13:59:06 +0800

    transmit about RecipeName, batchSize

--- src/UI/ConfirmBrewPage.java
+++ src/UI/ConfirmBrewPage.java
@@ -49,7 +49,7 @@ public class ConfirmBrewPage extends JFrame {
 				if(check) {
 					JOptionPane.showMessageDialog(null, "Success!");
 					closeThis();
-					new NotesWritePage().setVisible(true);
+					new NotesWritePage(Name,batchSize).setVisible(true);
 				}else {
 					JOptionPane.showMessageDialog(null,"Fail!","Warning",JOptionPane.ERROR_MESSAGE);
 					closeThis();
