d876488774d864e274b94bd8008796b66176477c
Yvonne <47659990+YvonneTech@users.noreply.github.com>
2019-05-15 16:21:08 +0800

    StorageIngredient: add comments

--- src/mainPart/StorageIngredient.java
+++ src/mainPart/StorageIngredient.java
@@ -1,8 +1,7 @@
 package mainPart;
 
-public class StorageIngredient extends Ingredient{
-	
-	
+public class StorageIngredient extends Ingredient{	
+	// StorageIngredient is a child class of ingredient
 	public StorageIngredient (String name, float quantity, String unit) {
 		super(name, quantity, unit);
 	}
@@ -11,8 +10,11 @@ public class StorageIngredient extends Ingredient{
 		// only accept non-negative numbers
 		if(quantity >= 0)
 		{
+			// get the original quantity from the database
 			float oldQuantity = Database.dbGetStorageingredientQuantity(ingredientName);
+			// compute the new quantity
 			quantity = oldQuantity + quantity;
+			// update the quantity in the database
 			return Database.dbUpdateStorageIngredient(ingredientName, quantity);
 		}
 		// if number is negative, then amount unchanged
@@ -26,18 +28,23 @@ public class StorageIngredient extends Ingredient{
 		{
 			float oldQuantity = Database.dbGetStorageingredientQuantity(ingredientName);
 			if (oldQuantity - quantity >= 0) {
+				// compute the new quantity
 				quantity = oldQuantity - quantity;
+				// update the quantity in the database
 				return Database.dbUpdateStorageIngredient(ingredientName, quantity);
 			} else {
+				// if the subtract quantity is larger than the original quantity
 				System.out.println("Subtract too much!");
 			}
-		} else { // if number is negative, then amount unchanged
+		} else {
+			// if number is negative, then amount unchanged
 			System.out.println("Invalid amount!");
 		}
 		return false;
 	}
 	
 	public static float searchQ(String name) {
+		// call this function to get the specific quantity of ingredient in storage
 		float Quantity = Database.dbGetStorageingredientQuantity(name);
 		return Quantity;
 	}
